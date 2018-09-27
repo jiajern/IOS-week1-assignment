050faae8002ee2ca85891c9b3b1b617c2c44ca8e
Alan Lau <alanlau97@gmail.com>
2018-09-27 12:46:11 -0400

    Initial Commit

---
+++ IOS-week1-assignmentTests/IOS_week1_assignmentTests.swift
@@ -0,0 +1,36 @@
+//
+//  IOS_week1_assignmentTests.swift
+//  IOS-week1-assignmentTests
+//
+//  Created by Alan Lau on 9/27/18.
+//  Copyright © 2018 Alan Lau. All rights reserved.
+//
+
+import XCTest
+@testable import IOS_week1_assignment
+
+class IOS_week1_assignmentTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
