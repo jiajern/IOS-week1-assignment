85bf8d8334ac13317ab72b66ea8c6d7bbef1b10e
Alan Lau <alanlau97@gmail.com>
2018-09-27 15:33:22 -0400

    tableview + data

--- IOS-week1-assignment/NowPlayingViewController.swift
+++ IOS-week1-assignment/NowPlayingViewController.swift
@@ -12,6 +12,7 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var movies: [[String: Any]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,22 +34,25 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
                 // TODO: Store the movies in a property to use elsewhere
                 // TODO: Reload your table view data
                 let movies = dataDictionary["results"] as! [[String: Any]]
-                for movie in movies {
-                    let title = movie["title"] as! String
-                    print(title)
+                self.movies = movies
+                self.tableView.reloadData()
                 }
-                
             }
-        }
         task.resume()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        
         return cell
     }
 
